ab55ae6681c0f4764341902a430162e0aa61811f
WillOfHeaven <113263307+WillOfHeaven@users.noreply.github.com>
2023-05-21 16:48:29 +0530

    changes made to package lock.json

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import Home from './Home';
 import Notification from './Notification';
 import Post from './Post';
-
+//import { Router, Switch, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      {/* <Home/> */}
-      {/* <Notification /> */}
-      <Post />
-    </div>
-  );
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/post" element={<Post/>}/>
+      <Route exact path="/home" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+);
 }
 
 export default App;
